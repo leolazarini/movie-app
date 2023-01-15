99174764471c90d0a9b95ef0c6fcfc719cd264fa
Leonardo Lazarini <leo.lazarini@gmail.com>
2023-01-15 17:16:53 -0300

    Initial Commit

---
+++ movie-app/movie_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_appApp.swift
+//  movie-app
+//
+//  Created by Leonardo Filipe Lazarini on 15/01/23.
+//
+
+import SwiftUI
+
+@main
+struct movie_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
